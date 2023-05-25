a938e1166616072635077b7b19f84babd4d892ca
unknown <shaima96alabri@hotmail.com>
2023-05-25 00:16:45 -0700

    add extand code in TeacherRepository

--- src/main/java/com/example/School/Management/System/Repositories/TeacherRepository.java
+++ src/main/java/com/example/School/Management/System/Repositories/TeacherRepository.java
@@ -1,4 +1,9 @@
 package com.example.School.Management.System.Repositories;
 
-public interface TeacherRepository {
+import com.example.School.Management.System.Models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher,Long> {
 }
